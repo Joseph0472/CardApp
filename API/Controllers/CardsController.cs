52e6bbbd153ba8e0c37e7af9e2d35aa631e6e337
Ajon <jiangpeng0472@gmail.com>
2023-09-21 11:36:06 +1200

    Add card endpoint

--- API/Controllers/CardsController.cs
+++ API/Controllers/CardsController.cs
@@ -1,4 +1,7 @@
-﻿using API.Data;
+﻿using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,7 +17,7 @@ public class CardsController : BaseApiController
         _context = context;
     }
 
-    [HttpGet]
+    [HttpGet] // Get all stored cards: /api/cards
     public async Task<ActionResult<IEnumerable<Card>>> GetCards()
     {
         var cards = await _context.Cards.ToListAsync();
@@ -22,9 +25,32 @@ public class CardsController : BaseApiController
         return cards; 
     }
     
-    [HttpGet("{id}")] // /api/card/2
+    [HttpGet("{id}")] // Get a card by Id /api/card/1
     public async Task<ActionResult<Card>> GetCard(int id)
     {
         return await _context.Cards.FindAsync(id);
+    }
+
+    [HttpPost("add")] // POST: api/account/register
+    public async Task<ActionResult<Card>> Register(
+        string creditNumber, 
+        string cvc,
+        int expiryDateMonth,
+        int expiryDateDay
+        )
+    {
+        using var hmac = new HMACSHA512();
+        var card = new Card
+        {
+            CreditNumber = creditNumber,
+            CVCHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(cvc)),
+            CVCSalt = hmac.Key,
+            ExpiryDateMonth = expiryDateMonth,
+            ExpiryDateDay = expiryDateDay
+        };
+
+        _context.Cards.Add(card);
+        await _context.SaveChangesAsync();
+        return card;
     }
 }
