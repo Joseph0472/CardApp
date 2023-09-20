1e6f411c268d0632776330eb08cbf9070cf79851
Ajon <jiangpeng0472@gmail.com>
2023-09-21 10:21:22 +1200

    Init and set entities

---
+++ API/Entities/Card.cs
@@ -0,0 +1,9 @@
+﻿namespace API;
+
+public class Card
+{
+    public int Id { get; set; }
+    public string CreditNumber { get; set; }
+    public string CVC { get; set; }
+    public ExpiryDate ExpiryDate { get; set; }
+}
---
+++ API/Entities/ExpiryDate.cs
@@ -0,0 +1,7 @@
+﻿namespace API;
+
+public class ExpiryDate
+{
+    public int Month { get; set; }
+    public int Day { get; set; }
+}
