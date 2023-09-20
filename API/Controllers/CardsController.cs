4f254e507c6d4a1db9adccc18b7701fe3056e30c
Ajon <jiangpeng0472@gmail.com>
2023-09-21 10:56:12 +1200

    Api running

---
+++ API/Controllers/CardsController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/cards
+public class CardsController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public CardsController(DataContext context)
+    {
+        _context = context;
+    }
+
+        [HttpGet]
+    public ActionResult<IEnumerable<Card>> GetUsers()
+    {
+        var cards = _context.Cards.ToList();
+        
+        return cards; 
+    }
+    
+    [HttpGet("{id}")] // /api/user/2
+    public ActionResult<Card> GetCard(int id)
+    {
+        return _context.Cards.Find(id);
+    }
+}
